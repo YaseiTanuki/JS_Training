802106d4dd04380b80e291ba45906fe16935ee89
YaseiTanuki <tanuki1707@gmail.com>
2025-07-24 08:25:20 +0700

    step 10

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -1,24 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], (Controller, MessageToast, JSONModel) => {
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
-        onInit() {
-            // set data model on view
-            const oData = {
-                recipient : {
-                    name : "World"
-                }
-            };
-            const oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-        },
-
         onShowHello() {
-            MessageToast.show("Hello World");
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+            // show message
+            MessageToast.show(sMsg);
         }
     });
 });
